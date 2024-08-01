934c3aac873923e8cce9a03cf07b06cb10400af5
suginotaisei629 <sugino.t29@gmail.com>
2024-08-01 13:05:17 +0900

    修正しました

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -1,35 +1,32 @@
 package basicSyntax;
 
 public class ForWhile {
-
 	public static void main(String[] args) {
-	 for (int num = 1; num <= 100; num++) {
-	   if (num % 3 == 0 && num % 5 == 0) {
-	        System.out.println("FizzBuzz");
-	    } else if (num % 3 == 0) {
-	         System.out.println("Fizz");
-	    } else if (num % 5 == 0) {
-	         System.out.println("Buzz");
-        } else {
-	         System.out.println(num);
-        }
-	 }
-	 
-	 String[] languages = {"Ruby", "Golang", "Java", "Python", "TYpescript", "HTML", "Javascript"};
-	 for (String lang : languages) {
-		 switch (lang) {
-		 case "Java":
-			 System.out.println("現在学習中の言語はJavaです。");
+		for (int num = 1; num <= 100; num++) {
+			if (num % 3 == 0 && num % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (num % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (num % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(num);
+			}
+		}
+			
+	String[] languages = {"Ruby", "Golang", "Java", "Python", "TYpescript", "HTML", "Javascript"};
+		for (String lang : languages) {
+		switch (lang) {
+			case "Java":
+				System.out.println("現在学習中の言語はJavaです。");
 			 continue;
-			 
-		 case "HTML":
-			 System.out.println("HTMLはプログラミング言語ではありません。");
+			case "HTML":
+				System.out.println("HTMLはプログラミング言語ではありません。");
 			 break;
-			 
-	     default:
-	    	 System.out.println(lang);
-	    	 break;
+			 default:
+		    	 System.out.println(lang);
+		     break;
+			 }
 		 }
-	 }
-}
+	}
 }
