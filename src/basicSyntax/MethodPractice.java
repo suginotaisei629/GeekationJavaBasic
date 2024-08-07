cb6296145546dfeff185b3ec6d592ad6e39741bd
suginotaisei629 <sugino.t29@gmail.com>
2024-08-07 11:06:02 +0900

    修正しましたー１

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -2,44 +2,44 @@ package basicSyntax;
 
 public class MethodPractice {
 
-	public static int add(int a, int b) {
-		return a + b;
-	}
-	
-	public static int subtract(int a, int b) {
-		return a - b;
-	}
+public static int add(int a, int b) {
+	return a + b;
+}
 
-	public static int multiply(int a, int b) {
-        return a * b;
+public static int subtract(int a, int b) {
+	return a - b;
+}
+
+public static int multiply(int a, int b) {
+	return a * b;
+}
+
+public static int divide(int a, int b) {
+	if (b == 0) {
+		System.out.println("Error");
+		return 0;
 	}
+	return a / b;
+}
+
+public static void print(int result) {
+	System.out.println("計算結果は" + result + "です。");
+}
+
+public static void main(String[] args) {
+	int a = 10;
+	int b = 5;
 	
-	public static int divide(int a, int b) {
-		if (b == 0) {
-			 System.out.println("Error");
-			 return 0;
-		}
-		return a / b;
-	}
+	int result1 = add(a,b);
+	print(result1);
 	
-	public static void print(int result) {
-		System.out.println("計算結果は" + result + "です。");
-	}
+	int result2 = subtract(a,b);
+	print(result2);
 	
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 5;
-		int result1 = add(a,b);
-		print(result1);
-		
-		int result2 = subtract(a,b);
-		print(result2);
-		
-		int result3 = multiply(a,b);
-		print(result3);
-		
-		int result4 = divide(a,b);
-		print(result4);
-		
-	    }
-	}
+	int result3 = multiply(a,b);
+	print(result3);
+	
+	int result4 = divide(a,b);
+	print(result4);
+}
+}
